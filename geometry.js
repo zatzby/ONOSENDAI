abc2e2ceaedfa8a11216a26cec9d821f481da521
zach <zach@fastlnnode.com>
2023-09-26 15:00:35 -0700

    adjusted geometry, title

--- geometry.js
+++ geometry.js
@@ -1,4 +1,4 @@
 import { BoxGeometry } from 'three';
 
-export const noteGeometry = new BoxGeometry(5, 5, 5);
+export const noteGeometry = new BoxGeometry(10, 10, 10);
 // originally set at BoxGeometry(10, 10, 10)
